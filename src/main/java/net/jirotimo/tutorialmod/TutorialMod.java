cb075bec3a462a5e18df473374747ad7f2132022
JiroTimo <163426203+JiroTimo@users.noreply.github.com>
2024-09-08 10:14:50 +0200

    added a very important comment

--- src/main/java/net/jirotimo/tutorialmod/TutorialMod.java
+++ src/main/java/net/jirotimo/tutorialmod/TutorialMod.java
@@ -4,11 +4,10 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//very important comment
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "Tutorialmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
 	}
